faa371136c63b23a66f9350a67e97b5e3e4aecb9
vanisudhi <vanishree164@gmail.com>
2018-03-18 18:22:23 +0530

    uploading framework

---
+++ src/test/java/generic/AutoConst.java
@@ -0,0 +1,12 @@
+package generic;
+
+public interface AutoConst
+{
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="./src/test/resources/chromedriver.exe";
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="./src/test/resources/geckodriver.exe";
+	String INPUT_PATH="./data/input.xlsx";
+
+
+}
